a60bc94b08ea670a97c78d7ef213ad571d5d87f4
Roman Bystrov <btstrov@gmail.com>
2023-02-14 18:32:37 +0300

    renaming

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import make_response
-import io
 
 from generator import Placeholder
 
@@ -10,10 +9,10 @@ app = Flask(__name__)
 @app.route('/<w>x<h>.<e>')
 def get_placeholder(w: int, h: int, e: str):
 
-    g = Placeholder(int(w), int(h), e)
+    placeholder = Placeholder(int(w), int(h), e)
 
-    response = make_response(g.to_bytes())
+    response = make_response(placeholder.to_bytes())
     response.headers['Content-Type'] = f'image/{e}'
-    response.headers['Content-Length'] = len(g.to_bytes())
+    response.headers['Content-Length'] = len(placeholder.to_bytes())
 
     return response
