57797841e37858d41fa664c70899a1c271cd39d8
Roman Bystrov <btstrov@gmail.com>
2023-02-14 18:29:07 +0300

    refactoring

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import make_response
 import io
 
-from generator import Generator
+from generator import Placeholder
 
 app = Flask(__name__)
 
@@ -10,16 +10,10 @@ app = Flask(__name__)
 @app.route('/<w>x<h>.<e>')
 def get_placeholder(w: int, h: int, e: str):
 
-    g = Generator(int(w), int(h))
-    placeholder = g.generate()
+    g = Placeholder(int(w), int(h), e)
 
-    raw_image = io.BytesIO()
-    placeholder.save(raw_image, format=e)
-    raw_image = raw_image.getvalue()
-
-    response = make_response(raw_image)
+    response = make_response(g.to_bytes())
     response.headers['Content-Type'] = f'image/{e}'
-    response.headers['Content-Length'] = len(raw_image)
-    response.data = raw_image
+    response.headers['Content-Length'] = len(g.to_bytes())
 
     return response
--- generator.py
+++ generator.py
@@ -1,23 +1,62 @@
-from typing import Self
+from typing import Literal, Tuple
+import io
 
 from PIL import Image as ImageT
 from PIL import ImageDraw, ImageFont
 from PIL.Image import Image
 
 
-class Generator:
-    def __init__(self, w: int, h: int) -> None:
-        self.width = w
-        self.height = h
+class AbstractPlaceholder(object):
+    placeholder_width: int
+    placeholder_height: int
+    placeholder_format: Literal['jpeg', 'png']
 
-    def generate(self) -> Image:
-        message = f'{self.width} x {self.height}'
+    def __generate(self) -> Image:
+        raise NotImplementedError
 
-        image: Image = ImageT.new(mode='RGB', size=(
-            self.width, self.height), color='gray')
+    def get_size(self) -> Tuple[str, str]:
+        return self.placeholder_width, self.placeholder_height
+
+    def get_message(self) -> str:
+        w, h = self.get_size()
+        return '{}x{}'.format(w, h)
+
+    def calculate_to_center(self, text_size: Tuple[int, int]) -> Tuple[int, int]:
+        tx, ty = text_size
+        width, height = self.get_size()
+
+        size = (width - tx) / 2, (height - ty) / 2
+
+        return size
+
+    def to_bytes(self) -> bytes:
+        raise NotImplementedError
+
+    def __bytes__(self) -> bytes:
+        return self.to_bytes()
+
+
+class Placeholder(AbstractPlaceholder):
+    def __init__(self, width: int, height: int, format: Literal['jpeg', 'png']) -> None:
+        self.placeholder_width = width
+        self.placeholder_height = height
+        self.placeholder_format = format
+
+        self.__generate()
+
+    def __generate(self) -> Image:
+        message = self.get_message()
+
+        self.placeholder_image = ImageT.new(
+            mode='RGB', size=self.get_size(), color='gray')
+
         font = ImageFont.truetype('arial', size=36)
-        sx, sy = font.getsize(message)
-        pencil = ImageDraw.Draw(image)
-        pencil.text(((self.width - sx) / 2, (self.height - sy) / 2),
-                    font=font, fill='black', size=36, text=message, align='center')
-        return image
+        pencil = ImageDraw.Draw(self.placeholder_image)
+
+        pencil.text(self.calculate_to_center(font.getsize(message)), font=font,
+                    fill='black', size=36, text=message, align='center')
+
+    def to_bytes(self) -> bytes:
+        raw_image = io.BytesIO()
+        self.placeholder_image.save(raw_image, format=self.placeholder_format)
+        return raw_image.getvalue()
